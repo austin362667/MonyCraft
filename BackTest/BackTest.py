83b870736a58ab832bdbb84ba3a94c2b863570b5
Austin Liu <austin362667@gmail.com>
2019-11-18 20:10:42 +0800

    add_code_001

---
+++ BackTest/BackTest.py
@@ -0,0 +1,20 @@
+import ffn
+import random 
+import math
+
+
+def RandomWalk(code):
+    
+    prices = ffn.get(str(code))
+
+    init = prices.iloc[-1][0]
+    return_daily = []
+    n_lst = [init]
+    for n in range(250):
+
+        n_lst.append(init*(random.random()-0.5)*math.sqrt(1/250))
+        return_daily.append(sum(n_lst))
+        
+    return return_daily
+    
+    
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'MonyCraft',
+    packages = ['BackTest'],
+    scripts = [],
+    version = '1.0',
+    description = 'A python quant platform for financial use.',
+    author = 'austin362667',
+    author_email = 'austin362667@gmail.com',
+    url = 'https://github.com/austin362667/MonyCraft',
+    download_url = 'https://github.com/austin362667/MonyCraft.git',
+    keywords = ['finance', 'backtest', 'quant'],
+    classifiers = [],
+)
